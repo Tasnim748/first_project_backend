08d37966c6017b8ae7932cda23c4462051b79952
Tasnim Jakir <tasnim748@gmail.com>
2023-05-13 23:14:41 +0600

    crud done hopefully

--- routers/imageRouter.js
+++ routers/imageRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Image } = require('../models/Carousel_Img');
 const router = express.Router();
+const DeleteFile = require('./fileDelete');
 
 const getImages = async (req, res) => {
     if (req.params.company) {
@@ -21,12 +22,32 @@ const deleteImage = async (req, res) => {
     console.log(req.body._id);
     try {
         await Image.findByIdAndDelete(req.body._id);
-        console.log('deleted', req.body._id);
+        console.log('deleted');
+        const url = req.body.url
+        const filename = url.substring(url.lastIndexOf('/') + 1);
+        console.log(filename);
+        DeleteFile(filename, res)
         return res.status(204).send('deletion successful');
     } catch(e) {
         console.log('some wrong')
-        return res.send('deletion error!');
-    } 
+        return res.status(500).send('deletion error!');
+    }
+}
+
+const updateImage = async (req, res) => {
+    console.log(req.body, req.body.url, req.body._id);
+    if (req.body.oldUrl) {
+        const url = req.body.oldUrl;
+        const filename = url.substring(url.lastIndexOf('/') + 1);
+        DeleteFile(filename, res);
+        // console.log('oldurl', req.body.oldUrl);
+    }
+    await Image.findByIdAndUpdate(req.body._id, {
+        title: req.body.title,
+        description: req.body.description,
+        url: req.body.url
+    })
+    return res.status(204).send('updated!');
 }
 
 router.route('/:company')
@@ -36,5 +57,6 @@ router.route('/')
     .get(getImages)
     .post(newImage)
     .delete(deleteImage)
+    .put(updateImage);
 
 module.exports = router;
